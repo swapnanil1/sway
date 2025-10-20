460da720deb1ab92fdffc4d8402674be6b761c5b
Swapnanil <135691331+swapnanil1@users.noreply.github.com>
2025-10-20 18:43:51 +0530

    Add interactive Fish shell configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    set -U fish_greeting ""
+
+    alias ls='eza -al --color=always --group-directories-first'
+    alias pi='sudo pacman -S'
+    alias pr='sudo pacman -R'
+    alias prr='sudo pacman -Rns'
+    function sudo
+        faillock --reset
+        env WAYLAND_DISPLAY=$WAYLAND_DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR sudo $argv
+    end
+end
