c1acbd79e123666ed90328fe77aec9dcb4b31f70
Swapnanil <135691331+swapnanil1@users.noreply.github.com>
2025-10-20 23:35:17 +0530

    Add alias for wofi launcher in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ if status is-interactive
     alias pi='sudo pacman -S'
     alias pr='sudo pacman -R'
     alias prr='sudo pacman -Rns'
+    alias wofi='wofi --show drun'
     function sudo
         faillock --reset
         env WAYLAND_DISPLAY=$WAYLAND_DISPLAY XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR sudo $argv
